9e1db52a97f84f568c6caa062bd814bf8dfa1cd4
Emma Smedley <emmasmedley@Emmas-MacBook-Pro.local>
2022-02-28 11:00:15 +0000

    conditionals work

---
+++ pycharm-projects/exercise-12- conditionals/lists.py
@@ -0,0 +1,11 @@
+# cheese = ["Chedder", "Stilton",  "Cornish Yarg"]
+# print(cheese)
+# add one cheese to the end of a list
+# cheese.append("Oke")
+# print(cheese)
+
+cheese = ["Chedder", "Stilton",  "Cornish Yarg"]
+print(cheese)
+# added two cheeses with a singel command
+cheese.extend(["Oke", "Halloumi"])
+print(cheese)
